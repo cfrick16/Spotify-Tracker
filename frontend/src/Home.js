22d6527b2343be26ada600062b2f9c33ee8a6139
cfrick16 <cfrick16@gmail.com>
2018-11-15 18:03:33 -0600

    Added simple REACT

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>HEY</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
